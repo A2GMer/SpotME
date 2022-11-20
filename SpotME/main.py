05426054590a07a24eef618102e4f5a41715117e
A2GMer <52153162+A2GMer@users.noreply.github.com>
2022-11-20 22:44:37 +0900

    sample

--- SpotME/main.py
+++ SpotME/main.py
@@ -25,30 +25,48 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 def hello_world():
     return "hello world!"
 
+## 1 ##
+#Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
+    # リクエストヘッダーから署名検証のための値を取得します。
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+    # リクエストボディを取得します。
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
     # handle webhook body
     try:
         handler.handle(body, signature)
+
+        # 署名検証で失敗した場合、例外を出す。
     except InvalidSignatureError:
         abort(400)
 
+    # handleの処理を終えればOK
     return 'OK'
 
+## 2 ##
+###############################################
+#LINEのメッセージの取得と返信内容の設定(オウム返し)
+###############################################
+#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
+#def以下の関数を実行します。
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        #ここでオウム返しのメッセージを返します。
+        TextSendMessage("hogehoge"))
 
 if __name__ == "__main__":
 #    app.run()
+    # ポート番号の設定
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
