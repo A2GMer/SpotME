e92618ce7527eada07c7355344a9e4e177cfb690
A2GMer <52153162+A2GMer@users.noreply.github.com>
2022-11-22 11:15:27 +0900

    複数行送信対応

--- SpotME/main.py
+++ SpotME/main.py
@@ -77,7 +77,7 @@ def handle_message(event):
     
     # 複数行対応のため、改行コードでSplit
     for l in recievedMessage.split('\n'):
-        print(m)
+        print(l)
         # 起動キーワードを検知
         rtn, m = is_execute(l)
         if rtn == False:
